da8a596f609f7f3aaeeece81bda16b0bf59b7f17
enamwit <enamwit@qq.com>
2024-09-08 22:18:06 +0800

    test one

--- 刷题用/源.cpp
+++ 刷题用/源.cpp
@@ -1,28 +1,9 @@
 #include<iostream>
 #include<vector>
-#include<map>
-#include<algorithm>
+#include<math.h>
 using namespace std;
 int main()
 {
-	int n;
-	cout << "ÊäÈën" << endl;
-	cin >> n;
-	map<int,int>a;
-	vector<int>b(n);
-	for(int i=0;i<n;i++)
-	{
-		cin >> b[i];
-	}
-	for (int i = 0; i < n; i++) {
-		
-		if (9 - b[i] > 0) { pair<int, int>shu(b[i], 9 - b[i]); a.insert(shu); }
-	}
-	for(int i=0;i<n;i++)
-	{if(b[i]==) }
-	cout << a[5];
-	cout << a[4];
-	cout << a[3];
-	return 0;
-
+	cout << "hello world" << endl;
+	return 0
 }
